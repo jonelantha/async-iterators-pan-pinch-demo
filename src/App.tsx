676d3464d08e3cbe845c67604b1f22785ffe70c7
jonelantha <57597570+jonelantha@users.noreply.github.com>
2024-10-24 20:05:14 +0100

    Step 2 - Pan - initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { useAsyncIterator } from "./util/useAsyncIterator";
+import { getEvents } from "./util/getEvents";
+
 import "./App.css";
 
 function App() {
+  const [container, setContainer] = useState<HTMLElement | null>(null);
+
+  const transform = useAsyncIterator(
+    async function* transforms(signal) {
+      const events = getEvents(container, ["pointermove"]);
+
+      for await (const event of events) {
+        yield new DOMMatrix().translate(event.x, event.y);
+      }
+    },
+    [container],
+  );
+
   return (
-    <div id="container">
-      <img />
+    <div id="container" ref={setContainer}>
+      <img style={{ transform: transform?.toString() }} />
     </div>
   );
 }
