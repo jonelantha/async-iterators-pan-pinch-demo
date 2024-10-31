a4b5ef1307e0d723ba6b73a74bd4714d9fc39821
jonelantha <57597570+jonelantha@users.noreply.github.com>
2024-10-31 13:45:25 +0000

    Step 2.1 - Pan - full implementation

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,47 @@ function App() {
 
   const transform = useAsyncIterator(
     async function* transforms(signal) {
-      const events = getEvents(container, ["pointermove"]);
+      const events = getEvents(container, [
+        "pointermove",
+        "pointerdown",
+        "pointerup",
+        "pointercancel",
+      ]);
+
+      let transform = new DOMMatrix();
+
+      let currentPointer: Pointer | undefined;
 
       for await (const event of events) {
-        yield new DOMMatrix().translate(event.x, event.y);
+        if (currentPointer === undefined) {
+          if (event.type === "pointerdown" && event.button === 0) {
+            event.preventDefault();
+
+            container!.setPointerCapture(event.pointerId);
+
+            currentPointer = { id: event.pointerId, x: event.x, y: event.y };
+          }
+        } else {
+          // currentPointer !== undefined
+          if (event.pointerId === currentPointer.id) {
+            if (event.type === "pointermove") {
+              const oldPointer = currentPointer;
+
+              currentPointer = { id: event.pointerId, x: event.x, y: event.y };
+
+              transform = transform.translate(
+                currentPointer.x - oldPointer.x,
+                currentPointer.y - oldPointer.y,
+              );
+
+              yield transform;
+            }
+
+            if (event.type === "pointerup" || event.type === "pointercancel") {
+              currentPointer = undefined;
+            }
+          }
+        }
       }
     },
     [container],
