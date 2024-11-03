732b3ce6e444b121ba3b3e03e138addac7fc680c
jonelantha <57597570+jonelantha@users.noreply.github.com>
2024-11-03 09:17:44 +0000

    Step 3 - Pinch

--- src/inputEvents.ts
+++ src/inputEvents.ts
@@ -10,6 +10,8 @@ export async function* inputEvents(element: HTMLElement | null) {
     "pointercancel",
   ]);
 
+  const elementCenter = getCenter(element);
+
   for await (const event of events) {
     switch (event.type) {
       case "pointerdown":
@@ -18,12 +20,35 @@ export async function* inputEvents(element: HTMLElement | null) {
 
           element.setPointerCapture(event.pointerId);
 
-          yield event;
+          yield offsettedPointerEvent(event, elementCenter);
         }
+        break;
+
+      case "pointermove":
+      case "pointerup":
+      case "pointercancel":
+        yield offsettedPointerEvent(event, elementCenter);
+
         break;
 
       default:
         yield event;
     }
   }
+}
+
+// helpers
+
+function getCenter(element: HTMLElement) {
+  const rect = element.getBoundingClientRect();
+
+  return { x: rect.width / 2, y: rect.height / 2 };
+}
+
+function offsettedPointerEvent(pointerEvent: PointerEvent, offset: Point) {
+  return new PointerEvent(pointerEvent.type, {
+    pointerId: pointerEvent.pointerId,
+    clientX: pointerEvent.clientX - offset.x,
+    clientY: pointerEvent.clientY - offset.y,
+  });
 }
